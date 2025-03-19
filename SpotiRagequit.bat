cc4339223c032e591ab45f0333c0501e1a4f0f44
TheCuriousCorvid <miller.a.jeremiah@gmail.com>
2025-03-19 11:46:18 -0700

    Update SpotiRagequit.bat

--- SpotiRagequit.bat
+++ SpotiRagequit.bat
@@ -1,6 +1,6 @@
 title SpotiRagequit v0.0.1
 taskkill /f /im Spotify.exe
 timeout 1
-start %appdata%\Spotify\Spotify.exe
+start "C:\Users\mille\AppData\Local\Packages\SpotifyAB.SpotifyMusic_zpdnekdrzrea0\RoamingState"
 timeout 1
 powershell -c "$wshell = New-Object -ComObject wscript.shell; $wshell.AppActivate('Spotify'); $wshell.SendKeys(' ')"
